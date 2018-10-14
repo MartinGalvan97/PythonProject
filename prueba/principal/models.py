371a6b673ce0dfd66982606ef1cd837a87163f1f
Martin Galvan <31630650+MartinGalvan6@users.noreply.github.com>
2018-10-13 17:12:17 -0700

    Nueva tabla de usuarios

--- prueba/principal/models.py
+++ prueba/principal/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class DatosGenerales(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField()
+
+class Usuarios(models.Model):
+    UserName = models.CharField(max_length=50)
